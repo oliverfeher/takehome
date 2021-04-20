68887a8199c7feec4e7411f7fad5bef5f346e9a7
Oliver Feher <olcsee@gmail.com>
2021-04-19 21:33:37 -0400

    loading test data.

---
+++ src/data-module.js
@@ -0,0 +1,41 @@
+const customerReviewsData = {
+    maScore: 4.8,
+    title: 'Read what some customers of the Tuft & Needle Hybrid have to say.',
+    reviews: [
+        {
+            quote: '<p>I had horrible back pain from our previous mattress. Within a couple of nights sleeping on this mattress it was gone! <strong>I haven’t had a better night’s sleep in years.</strong><p>',
+            quoteSource: 'Michelle C.'
+        },
+        {
+            quote: '<p>Extremely <strong>comfortable and cooling</strong>, easy to set up. Made a huge difference in sleep quality!<p>',
+            quoteSource: 'Val A.'
+        },
+        {
+            quote: '<p>We never imagined that a mattress purchased online and delivered in a small box would be <strong>the best mattress we ever purchased.</strong><p>',
+            quoteSource: 'Matteo M.'
+        }
+    ]
+}
+
+const frequentlyAskedData = {
+    questions: [
+        {
+            question: 'What is the delivery window?',
+            answer: 'Beautyrest has partnered with XPO Logistics for delivery. It will take approximately 4-6 weeks for XPO to reach out to schedule your delivery day. They will send an email with instructions on how to schedule your delivery online. The day before the expected delivery, XPO will reach out to schedule a delivery window. Delivery times are scheduled in 4-hour increments. The typical delivery hours are (excluding major holidays): Monday through Saturday, 7am – 7pm.'
+        },
+        {
+            question: 'What is the 100-Night sleep trial?',
+            answer: 'If the mattress ends up not being the perfect fit for you within the first 100 nights, Beautyrest will help you with a return and full refund'
+        },
+        {
+            question: 'Can I finance my mattress?',
+            answer: 'Great sleep is in reach no matter your financial situation! If you prefer to pay for your Nectar mattress over a period of time, you can opt for 6 months of low, interest-free payments through Affirm. Simply select this option at checkout. No credit check is required and you can apply by using a bank account, credit, or debit card and proving a source of income.'
+        },
+    ],
+    ctas: [
+        {
+            link: 'https://www.randomlink.com',
+            title: 'more info'
+        }
+    ]
+}
