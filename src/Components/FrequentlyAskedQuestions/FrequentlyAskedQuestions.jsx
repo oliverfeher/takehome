43e6fcba265e062d774bb1bf660352975398d208
Haloren <mckennakyler@gmail.com>
2021-04-20 19:37:38 -0700

    added styling to FAQ more info button

--- src/Components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
+++ src/Components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './frequently-asked-questions.css';
-// import { Link } from 'react-router-dom';
 import { frequentlyAskedData } from '../../data-module.js';
 
 import rightArrow from '../../Assets/icons/right-arrow-white.png';
@@ -14,28 +13,29 @@ const FrequentlyAskedQuestions = () => {
         <div>
             <div className="faq-bg"></div>
             <div className="faq-container">
-            <h1>Frequently Asked Questions</h1>
-
-            {frequentlyAskedData.questions.map((question, index) =>
-                <div key={index}>
-                    { textToggle ?
-                        <h3 style={{ cursor: 'pointer' }} onClick={ () => textToggleState(!textToggle)}>+ {question.question}</h3>
-                        :
-                        <div>
-                            <h3 style={{ cursor: 'pointer' }} onClick={ () => textToggleState(!textToggle)}>- {question.question}</h3>
-                            <p>{question.answer}</p>
-                        </div>
-                    }
-                </div>
-            )}
-
-            {/* <Link to={frequentlyAskedData.ctas[0].link}> */}
-                <button>
-                    <a href={frequentlyAskedData.ctas[0].link}>{frequentlyAskedData.ctas[0].title}</a>
-                    <img src={rightArrow} alt="right arrow" />
-                </button>
-            {/* </Link> */}
-            <img src={faqRightImage} alt="bed with pillows" />
+                <h1>Frequently Asked Questions</h1>
+        
+                {/* use index to show individual answer onClick  */}
+                {frequentlyAskedData.questions.map((question, index) =>
+                    <div key={index}>
+                        { textToggle ?
+                            <h3 style={{ cursor: 'pointer' }} onClick={ () => textToggleState(!textToggle)}>+ {question.question}</h3>
+                            :
+                            <div>
+                                <h3 style={{ cursor: 'pointer' }} onClick={ () => textToggleState(!textToggle)}>- {question.question}</h3>
+                                <p>{question.answer}</p>
+                            </div>
+                        }
+                    </div>
+                )}
+    
+                    <a className="info-btn" href={frequentlyAskedData.ctas[0].link}>
+                        <button>
+                            {frequentlyAskedData.ctas[0].title}
+                            <img src={rightArrow} alt="right arrow" />
+                        </button>
+                    </a>
+                <img src={faqRightImage} alt="bed with pillows" />
             </div>
         </div>
     )
