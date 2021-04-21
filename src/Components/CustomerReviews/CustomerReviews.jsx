e2355146b6ce270a81bfe5a146ddb79cd9a151d6
Haloren <mckennakyler@gmail.com>
2021-04-20 23:08:51 -0700

    changed customer review font sizes

--- src/Components/CustomerReviews/CustomerReviews.jsx
+++ src/Components/CustomerReviews/CustomerReviews.jsx
@@ -9,9 +9,9 @@ import halfStar from '../../Assets/icons/half-star-yellow.svg';
 import fullStar from '../../Assets/icons/rating-star-yellow.svg';
 
 const CustomerReviews = () => {
-
-    let rating = customerReviewsData.maScore
-    let stars = [];
+    let rating = customerReviewsData.maScore || 0;
+    // let rating = 3.1 || 0;
+    let stars = [0,0,0,0,0];
 
     return (
         <div className="reviews-grid">
@@ -21,13 +21,19 @@ const CustomerReviews = () => {
                 <h1>Customer Reviews</h1>
 
                 {/* useState to fill in the stars based on maScore use case statements*/}
-                <h2 className="rating">{rating}/5
-                    <img src={emptyStar} alt="star"/>
-                    <img src={emptyStar} alt="star"/>
-                    <img src={emptyStar} alt="star"/>
-                    <img src={emptyStar} alt="star"/>
-                    <img src={emptyStar} alt="star"/>
-                </h2>
+                <div>
+                    <h2 className="rating">{rating}/5
+                        {stars.map((star, index) => {
+                            return(
+                            <div key={index}>
+                                <img src={emptyStar} alt="empty star"/>
+                                <img src={halfStar} alt="half star"/>
+                                <img src={fullStar} alt="full star"/>
+                            </div>
+                            )
+                        })}
+                    </h2>
+                </div>
 
                 <h4>{customerReviewsData.title}</h4>
                 <div className="review-wrapper">
